cbc3c9069d857b1fc308c76bd7cec871700464c4
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2022-03-24 12:28:50 +0100

    changed prints to logging; updated requireements; added wait for server

--- userspace/services/entrypoint.sh
+++ userspace/services/entrypoint.sh
@@ -3,6 +3,6 @@
 cd rasa
 ../.venv/bin/rasa run actions &
 ../.venv/bin/rasa run &
-sleep 120
 ../.venv/bin/python3 -m client &
+
 tail -f /dev/null
--- userspace/services/rasa/client/main.py
+++ userspace/services/rasa/client/main.py
@@ -1,10 +1,16 @@
 import json
-import traceback
+import logging
+import time
 
 import redis
+import requests
 import socketio
 
 from client import config as cfg
+from client import utils
+
+utils.config_logger("rasa")
+logger = logging.getLogger("rasa")
 
 
 def treat_input_cb(sio: socketio.Server):
@@ -19,7 +25,7 @@ def treat_input_cb(sio: socketio.Server):
 
         to_rasa = {"sender": cfg.RASA_SENDER_NAME, "message": data["message"]}
 
-        print(f"to send: {to_rasa}")
+        logger.debug("to send: %s", to_rasa)
 
         # asyncio.run(sio.emit(cfg.RASA_USER_MSG_EVT, to_rasa))
         sio.emit(cfg.RASA_USER_MSG_EVT, to_rasa)
@@ -30,58 +36,63 @@ def treat_input_cb(sio: socketio.Server):
 def register_sio_routes(sio, rc: redis.Redis):
     @sio.on(cfg.RASA_BOT_MSG_EVT)
     def receive_rasa_response(response):
-        print(f"from rasa: {response}")
+        logger.debug("from rasa: %s", response)
         rc.publish(cfg.RASA_REDIS_OUTPUT_CHANNEL, json.dumps(response))
-
 
     @sio.event
     def connect():
-        print("I'm connected!")
+        logger.info("Connected to socketio server.")
 
     @sio.event
     def connect_error(data):
-        print("The connection failed!")
-        print(data)
+        logger.error("Socketio connection failed. Data:\n%s", data)
 
     @sio.event
     def disconnect():
-        print("I'm disconnected!")
+        logger.info("Disconnected from socketio server.")
 
 
 def main():
+    # wait for rasa server to be running
+    logger.info("Waiting for RASA server to be up.")
+    code = 0
+    while code != 200:
+        try:
+            r = requests.get(cfg.RASA_SIO_SERVER_ADDR)
+            code = r.status_code
+        except requests.exceptions.RequestException:
+            time.sleep(1)
+    logger.info("RASA server running.")
+
     # setup redis
     rc = redis.Redis(
         cfg.REDIS_HOST, cfg.REDIS_PORT, password=cfg.REDIS_PASSWORD
     )
     rps = rc.pubsub()
 
-    # connected = False
-    # print("trying to connect to rasa sio server")
-    # while not connected:
-    #     try:
-    #     except Exception:
-    #         traceback.print_exc()
-    #         print("connection failed, retrying")
-    #     connected = sio.connected
     sio = socketio.Client()
 
-    print("connected")
-
     register_sio_routes(sio, rc)
+    logger.info("Registered socketio routes.")
 
     rps.subscribe(**{cfg.RASA_REDIS_INPUT_CHANNEL: treat_input_cb(sio)})
     # listen to messages in thread
     rps_thread = rps.run_in_thread(sleep_time=0.001)
+    logger.info("Listening to messages from Redis.")
 
     sio.connect(cfg.RASA_SIO_SERVER_ADDR)
 
     # program exits with error code 1 by default
     code = 1
     try:
+        logger.info("Running indefinitely.")
         sio.wait()
     except KeyboardInterrupt:
+        logger.error("KeyboardInterrupt received. Exiting.")
         # set exit code to 0 if keyboard interrupt
         code = 0
+    except Exception:
+        logger.error("Exception encountered.", exc_info=1)
     finally:
         rps_thread.stop()
         exit(code)
