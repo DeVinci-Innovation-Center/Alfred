0fc8edf1fe161ae0c2aeec10efb2bbec97fb97a9
Wim <poignon.wim@gmx.de>
2022-12-08 20:21:41 +0100

    feat: write from svg

---
+++ userspace/api/src/applications/modules/handwriting/trajectory_path.py
@@ -0,0 +1,81 @@
+import numpy as np
+import svgpathtools
+import xml.etree.ElementTree as ET
+
+from typing import List, Tuple, Union
+
+
+class TrajectoryPath:
+    def __init__(self, path: svgpathtools.path.Path, cartesian_equation: Tuple[float], z_height: float, nb_points: int = 2) -> None:
+        self.path = path
+        self.cartesian_equation = cartesian_equation
+        self.z_height = z_height
+        self.nb_points = nb_points
+        self.points = []
+        self.path_to_points()
+
+    def path_to_points(self) -> None:
+        """Set points from path"""
+        if self.nb_points < 2 or self.nb_points > 10:
+            self.nb_points = 2
+        a, b, c, d = self.cartesian_equation
+        next_segment = True
+        if len(self.path) != 0:
+            current_pos = None
+            for segment in self.path:
+                seg_start = segment.start
+                for i in np.linspace(0, 1, self.nb_points):
+                    x, y = round(segment.point(i).real, 2), round(
+                        segment.point(i).imag, 2)
+                    z = -1/c*(a*x+b*y+d) + self.z_height
+                    points = [x, y, z]
+                    if current_pos != seg_start and next_segment:
+                        points.append('M')
+                        next_segment = False
+                    self.points.append(points)
+                next_segment = True
+                current_pos = segment.end
+
+    def get_points(self):
+        """Get robot coordinates to move"""
+        return self.points
+
+
+class TrajectoryPaths:
+    def __init__(self, file_path: str, cartesian_equation: Tuple[float] = [0, 0, 1, -250], z_height: float = .0, svg_scale: float = 0.5, nb_points: int = 2) -> None:
+        self.file_path = file_path
+        self.z_height = z_height
+        self.cartesian_equation = cartesian_equation
+        self.nb_points = nb_points
+
+        root = ET.parse(file_path).getroot()
+        self.svg_width = int(root.attrib["width"])
+        self.svg_height = int(root.attrib["height"])
+
+        self.svg_scale = svg_scale
+        self.svg_translate = self.svg_width/2
+        self.svg_to_paths()
+
+    def svg_to_paths(self) -> None:
+        """Set all svg_paths"""
+        paths, _ = svgpathtools.svg2paths(self.file_path)
+        self.list_path = []
+        for element in paths:
+            element = element.scaled(-self.svg_scale, self.svg_scale)
+            element = element.translated(self.svg_translate)
+            self.list_path.append(TrajectoryPath(
+                element, self.cartesian_equation, self.z_height, self.nb_points))
+
+    def get_path_from_index(self, index: int) -> Union[TrajectoryPath, None]:
+        """Get specific TrajectoryPath with index
+
+        :param index: index
+        :type index: int
+        :return: Svg_path
+        :rtype: Union[TrajectoryPath,None]
+        """
+        try:
+            return self.list_path[index]
+        except Exception as e:
+            print(e)
+            return None
---
+++ userspace/api/src/applications/modules/handwriting/writing.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+
+from libalfred import AlfredAPI
+from .trajectory_path import TrajectoryPath, TrajectoryPaths
+from typing import Any, List
+
+
+def write_letter(arm:AlfredAPI, path:TrajectoryPath, init_pos:np.ndarray, z_offset:float)->None:
+    coords=path.get_points()
+    for coord in coords:
+        if coord[-1]=='M':
+            arm.set_position(z=z_offset,wait=True,speed=50,relative=True) #move up
+            arm.set_position(x=init_pos[0]+coord[0],y=init_pos[1]+coord[1],wait=True,speed=100)
+            arm.set_position(z=coord[2],speed=30, wait = True)
+            continue
+        arm.set_position(x=init_pos[0]+coord[0],y=init_pos[1]+coord[1], z=coord[2],wait=False,speed=30)
+    arm.set_position(z=z_offset,wait=True,speed=50,relative=True) #move up
+
+
+def write_from_file(arm:AlfredAPI, filename:str, init_pos:np.ndarray, pen_tip_height:float, cartesian_equation:np.ndarray, z_offset:float)->None:
+    paths = TrajectoryPaths(filename,cartesian_equation,pen_tip_height)
+    for path in paths.list_path:
+        write_letter(arm,path,init_pos,z_offset)
+    arm.set_position(*init_pos, wait=True, speed=100)
+
+def write_demo(word:str):
+    arm = AlfredAPI()
+    init_pos = np.array([35, 200,170, 180, -54.1, 77.6])
+    path = "src/applications/modules/handwriting/svg/"+word[0].lower()+".svg"
+    z_offset=10
+    pen_tip_height=-46
+    pen_tip_height=40
+    cartesian_equation = [20, -77, -2800, 546940]
+    arm.set_position(*init_pos, wait=True, speed=50, mvacc=10)
+    write_from_file(arm,path,init_pos,pen_tip_height,cartesian_equation,z_offset)
