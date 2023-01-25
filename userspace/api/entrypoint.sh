755c07ea0e06618847cbf5829109c3f893d32c35
Wim <poignon.wim@gmx.de>
2023-01-25 12:04:52 +0100

    fix(test): path

--- userspace/api/entrypoint.sh
+++ userspace/api/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-.venv/bin/python3 -m pytest &
-.venv/bin/uvicorn src.app:app --reload --host=0.0.0.0
+cd /alfred/api/src &&
+/alfred/api/.venv/bin/python3 -m pytest .. &
+cd /alfred/api/src &&
+/alfred/api/.venv/bin/uvicorn app:app --reload --host=0.0.0.0
