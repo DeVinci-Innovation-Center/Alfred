67d3404eaffaafa4a4bb35d675b93f1476f81dfe
Wim <poignon.wim@gmx.de>
2023-02-16 09:59:17 +0100

    Add UI module

--- userspace/api/src/applications/routers/demo.py
+++ userspace/api/src/applications/routers/demo.py
@@ -6,16 +6,18 @@ from libalfred import AlfredAPI
 from fastapi import APIRouter, HTTPException, status
 
 from applications.modules.gripper import gripper
-from applications.modules.handwriting import writing,plot
+from applications.modules.handwriting import writing, plot
 from utils.apps import App, AppRunningException, ctx_manager
 from utils.global_instances import sio as backend_sio_server
 
 from pydantic import BaseModel
 from typing import List, Union
 
+
 class Drawing(BaseModel):
     name: str
-    draw: Union[List[float], None] = None
+    draw_list: Union[List[float], None] = None
+
 
 router = APIRouter(prefix="/demo", tags=["Applications"])
 
@@ -38,15 +40,14 @@ async def demo_gripper():
 
     return {"message": "Grip an object."}
 
+
 @router.post("/write/{word}")
-async def write(word:str):
-    """Demo writing """
+async def write(word: str):
+    """Demo writing"""
     try:
         app = App(
-            use_sockets=False,
-            socket=backend_sio_server,
-            target=writing.write_demo,
-            f_args=([word],),
+            target=writing.write_demo_from_file,
+            f_args=(word,),
         )
         ctx_manager.run_app(app)
     except AppRunningException as e:
@@ -55,24 +56,22 @@ async def write(word:str):
         ) from None
 
     return {"message": "arm moves."}
-"""
-@router.post("/angle")
-async def get_angle():
-    #arm = AlfredAPI()
-    #p = arm.get_servo_angle()
-    #pos = ast.literal_eval(p)
-    return time.time()
-    if pos[0]==0:
-        servo_angle = [round(a,2) for a in pos[1][:-1]]
-        return {"message": "hey"}
-"""
+
 
 @router.post("/drawing/")
-async def get_draw(draw:Drawing):
-    draw_dic = draw.dict()
-    if draw.name!="" or draw.name is not None:
-        print(draw.name)
-        plot.plot(draw_dic)
-        return {"msg":"received"}
+async def get_draw(draw: Drawing):
+    if draw.name != "" or draw.name is not None:
+        try:
+            app = App(
+                target=writing.write_demo_from_ui,
+                f_args=(draw.draw_list,),
+            )
+            ctx_manager.run_app(app)
+        except AppRunningException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=e.message
+            ) from None
+
+        return {"msg": "received"}
     else:
-        return {"msg":"Fail"}
+        return {"msg": "Fail"}
